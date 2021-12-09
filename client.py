412c0d891dc4288ed8a1b92671982b800c53a86b
noamTzuber <noamtooby@gmail.com>
2021-12-09 21:55:23 +0200

    shrink for send list

--- client.py
+++ client.py
@@ -207,7 +207,7 @@ def send_identity(s):
 # ===============================================================================
 def send_list(s):
     global updates_list
-
+    shrink_modifies(update_list)
     empty_list = 0
     # move all the command in list
     for command in updates_list:
